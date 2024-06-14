3bedf45a0a608369f6c0ed6a30fd6899e549558a
Carlos Silva <carlosemilio.contato@gmail.com>
2024-06-14 11:26:58 -0300

    fix: page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { Routes } from './Routes'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import { ElectronProvider } from './utils/contexts/electron.context'
 import './styles/global.css'
 
-function App() {
+export function App() {
   return (
     <ElectronProvider>
-      <div className='w-screen h-screen bg-rotion-900 text-base-100 flex'>
-        <Sidebar />
-        <div className='flex-1 flex flex-col max-h-screen'>
-          <Header />
-          <Routes />
-        </div>
-      </div>
+      <Routes />
     </ElectronProvider>
   )
 }
-
-export default App
