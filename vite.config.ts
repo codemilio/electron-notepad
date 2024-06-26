f0b80ba670411b867e53ea65395e9634cc9afb51
Carlos Silva <carlosemilio.contato@gmail.com>
2024-06-25 22:18:33 -0300

    feat: config vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
+import tsConfigPathsPlugin from 'vite-tsconfig-paths'
+
+const tsconfigPaths = tsConfigPathsPlugin({
+  projects: [path.resolve('tsconfig.json')]
+}) 
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +16,17 @@ export default defineConfig({
       main: {
         // Shortcut of `build.lib.entry`.
         entry: 'electron/main.ts',
+        vite: {
+          plugins: [tsconfigPaths]
+        }
       },
       preload: {
         // Shortcut of `build.rollupOptions.input`.
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
         input: path.join(__dirname, 'electron/preload.ts'),
+        vite: {
+          plugins: [tsconfigPaths]
+        }
       },
       // Ployfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
