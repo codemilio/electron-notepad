8cd6704003bfd54cd375750b6294b54ef0d57edb
Carlos Silva <carlosemilio.contato@gmail.com>
2024-06-26 20:28:42 -0300

    feat: create interfaces and declare ipc methods

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,5 +1,6 @@
 import { ipcRenderer, contextBridge } from 'electron'
 import { IPC } from '~/src/utils/shared/constants/ipc.constants'
+import { CreateDocumentResponse, DeleteDocumentRequest, FetchDocumentRequest, FetchDocumentResponse, SaveDocumentRequest } from '~/src/utils/shared/types/ipc.types'
 
 // --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
@@ -24,5 +25,17 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
 // --------- Custom API exposed to the Renderer process ---------
 contextBridge.exposeInMainWorld('api', {
   getPlatform: () => ipcRenderer.invoke('get-platform'),
-  fetchDocuments: () => ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL)
+  fetchDocuments: () => ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL),
+  fetchDocument: (req: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, req)
+  },
+  createDocument: (): Promise<CreateDocumentResponse> => {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE)
+  },
+  saveDocument: (req: SaveDocumentRequest): Promise<void> => {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req)
+  },
+  deleteDocument: (req: DeleteDocumentRequest): Promise<void> => {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.DELETE, req)
+  },
 })
--- src/utils/shared/types/ipc.types.ts
+++ src/utils/shared/types/ipc.types.ts
@@ -1,13 +1,34 @@
 export type Document = {
   id: string
   title: string 
-  content: string 
+  content?: string 
 }
 
+/* 
+  @Response
+*/
 export type FetchAllDocumentsResponse = {
   data: Document[]
 }
 
-export type FetchAllDocumentsRequest = {
-  // 
+export type FetchDocumentResponse = {
+  data: Document
 }
+
+export type CreateDocumentResponse = {
+  data: Document
+}
+
+/* 
+  @Request
+*/
+export type FetchDocumentRequest = {
+  id: string
+}
+
+export type DeleteDocumentRequest = {
+  id: string
+}
+
+export type SaveDocumentRequest = Document
+
