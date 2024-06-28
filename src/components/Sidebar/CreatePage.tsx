2baa310a255347150acf784b456416f43ab02380
Carlos Silva <carlosemilio.contato@gmail.com>
2024-06-27 23:54:59 -0300

    feat: create document and update query state on sucess

--- src/components/Sidebar/CreatePage.tsx
+++ src/components/Sidebar/CreatePage.tsx
@@ -1,8 +1,32 @@
+import { useMutation } from '@tanstack/react-query'
+import { queryClient } from '../../utils/libs/rq.lib'
 import { Plus } from 'phosphor-react'
+import { Document } from '~/src/utils/shared/types/ipc.types'
 
 export function CreatePage() {
+  const { 
+    mutateAsync: createDocument,
+    isPending: isCreatingNewDocument, 
+  } = useMutation({
+    mutationFn: async () => {
+        const response = await window.api.createDocument()
+        return response.data
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(['documents'], async (documents: Document[]) => {
+        return [...documents, data]
+      })
+    }
+  })
+
+  const handleCreateDocument = () => createDocument()
+  
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-base-600 hover:bg-base-700 disabled:opacity-60">
+    <button 
+      onClick={handleCreateDocument}
+      disabled={isCreatingNewDocument}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-base-600 hover:bg-base-700 disabled:opacity-60"
+    >
       <Plus className="h-4 w-4" />
       Create new page
     </button>
